d61d9d270347937de4b1da628e1e8f334fd94152
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-30 01:34:06 +0530

    implement footer in main.jsx

--- client/CampusBeacon/src/components/Footer.jsx
+++ client/CampusBeacon/src/components/Footer.jsx
@@ -2,4 +2,9 @@ import React from "react";
 import {motion} from "framer-motion"
 
 const Footer = () =>{
-}
+    
+    return (
+        <div className="bg-gradient-to-b from gray-700 to-black text-white py-7"></div>
+    );
+};
+export default Footer;
--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -1,11 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import LoginSignup from "./pages/loginPage.jsx";
+import LoginSignup from "./pages/LoginPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LoginSignup />
+    <Footer />
   </StrictMode>
 );
