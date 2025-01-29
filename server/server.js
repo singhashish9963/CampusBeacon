4aed37ede9257717926de54303133652fa430a32
Ayush <ayushjadaun6@gmail.com>
2025-01-29 22:57:12 +0530

    Creating users table and successfull database connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 import express from "express"
-import dotenv from "dotnev"
+import dotenv from "dotenv"
+import sequelize from "./src/db/db.js";
+
+dotenv.config({path:"./.env"});
 
 const app=express();
 
 const PORT=process.env.PORT || 5000
+
+
+sequelize
+  .authenticate()
+  .then(()=>{
+    console.log("Connection has been established  successfully.");
+  })
+  .catch((err)=>{
+    console.error("Unable to connect to the database ", err)
+  });
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
