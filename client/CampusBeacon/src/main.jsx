61472eac98906bf421d6dd0e88c222dc663e61f6
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-30 01:52:14 +0530

    update main

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import LoginSignup from "./pages/LoginPage.jsx";
 import Footer from "./components/Footer.jsx";
+import App from "./App.jsx"
+import {BrowserRouter} from "react-router-dom"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <BrowserRouter>
     <LoginSignup />
     <Footer />
+    </BrowserRouter>
   </StrictMode>
 );
