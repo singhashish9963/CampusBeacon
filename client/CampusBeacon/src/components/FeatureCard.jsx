0076cd1f1c72a2a3fb1a05626ca07f2d95ee62ee
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-30 23:00:40 +0530

    add FeatureCard component

--- client/CampusBeacon/src/components/FeatureCard.jsx
+++ client/CampusBeacon/src/components/FeatureCard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const FeatureCard = () => {};
+const FeatureCard = ({ icon: Icon, title, descrip }) => {
+  return (
+    <motion.div
+      intial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      whileHover={{ scale: 1.25 }}
+      className="bg-gray-900/50 backdrop-blur-sm rounded-xl border border-pink-500/20 hover:border-pink-200/40 transition-all"
+    ></motion.div>
+  );
+};
 
 export default FeatureCard;
