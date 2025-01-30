f25a1bb7ad82a56b05d518e202ec4e2ad547a9e0
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-30 20:33:03 +0530

    refine login page styling

--- client/CampusBeacon/src/components/ButtonColourfull.jsx
+++ client/CampusBeacon/src/components/ButtonColourfull.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const ButtonColourfull = ({ text = "Error", type = "submit" }) => {
+const ButtonColourfull = ({ text = "Error", type = "submit" , textsize = "text-base", buttonsize = "w-full p-4"}) => {
   return (
     <motion.button
       text={text}
       type={type}
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.95 }}
-      className="w-full p-4 bg-gradient-to-r from-purple-600 to-pink-600 text-white rounded-lg font-semibold hover:opacity-90 transition-all"
+      className={`${buttonsize} text-m bg-gradient-to-r from-purple-600 to-pink-600 text-white rounded-lg font-semibold hover:opacity-90 transition-all ${textsize}`}
     >
       {text}
     </motion.button>
--- client/CampusBeacon/src/pages/loginPage.jsx
+++ client/CampusBeacon/src/pages/loginPage.jsx
@@ -17,9 +17,9 @@ function LoginSignup() {
       <motion.div
         inital={{ opacity: 0, scale: 0.7 }}
         animate={{ opacity: 1, scale: 0.8 }}
-        className="relative w-[1000px] h-[700px] rounded-2xl overflow-hidden flex"
+        className="relative w-[1100px] h-[750px] rounded-2xl overflow-hidden flex"
         style={{
-          background: "rgba(0,0,0,5)",
+          background: "rgba(0,0,0,0.7)",
           backdropFilter: "blur(50px)",
         }}
       >
@@ -54,8 +54,8 @@ function LoginSignup() {
         {/* Left side - Sign Up page */}
 
         <div className="w-1/2 p-15">
-          <h2 className="text-white text-4xl font-bold mb-7">
-            Create New Account
+          <h2 className="text-white text-5xl font-bold mb-7 mt-5">
+            Create Account
           </h2>
           <form onSubmit={handleSignUP} className="space-y-6">
             <motion.dv whileTap={{ scale: 0.98 }}>
@@ -63,7 +63,7 @@ function LoginSignup() {
                 type="email"
                 name="email"
                 placeholder="Enter your college email id"
-                className="mt-4 w-full p-4 text-xl bg-purple-500/15 rounded-lg text-white border border-white/11 focus-outline-none focus:border-purple-500 transition-all"
+                className="mt-4 w-full p-4 text-2xl bg-purple-500/15 rounded-lg text-white border border-white/11 focus-outline-none focus:border-purple-500 transition-all"
               />
             </motion.dv>
             <motion.div whileTap={{ scale: 0.98 }}>
@@ -71,23 +71,30 @@ function LoginSignup() {
                 type="password"
                 name="password"
                 placeholder="Create a password"
-                className="w-full mt-7 p-4 text-xl bg-purple-500/15 rounded-lg text-white"
+                className="w-full mt-7 p-4 text-2xl bg-purple-500/15 rounded-lg text-white"
               />
             </motion.div>
-            <ButtonColourfull text="Submit" type="submit" />
+            <ButtonColourfull
+              text="Submit"
+              type="submit"
+              textsize="text-xl"
+              buttonsize="w-1/2 p-4 ml-20 mt-10"
+            />
           </form>
         </div>
         {/* Right side - Log In Page */}
 
         <div className="w-1/2 p-15">
-          <h2 className="text-white text-4xl font-bold mb-7">Welcome Back</h2>
+          <h2 className="text-white text-5xl font-bold mb-7 mt-5">
+            Welcome Back
+          </h2>
           <form onSubmit={handleSignUP} className="space-y-6">
             <motion.dv whileTap={{ scale: 0.98 }}>
               <input
                 type="email"
                 name="email"
                 placeholder="Enter your college email id"
-                className="w-full mt-4 p-4 text-xl bg-purple-500/15 rounded-lg text-white border border-white/11 focus-outline-none focus:border-purple-500 transition-all"
+                className="w-full mt-4 p-4 text-2xl bg-purple-500/15 rounded-lg text-white border border-white/11 focus-outline-none focus:border-purple-500 transition-all"
               />
             </motion.dv>
             <motion.div whileTap={{ scale: 0.98 }}>
@@ -95,7 +102,7 @@ function LoginSignup() {
                 type="password"
                 name="password"
                 placeholder="Enter your password"
-                className="w-full mt-7 p-4 text-xl bg-purple-500/15 rounded-lg text-white"
+                className="w-full mt-7 p-4 text-2xl bg-purple-500/15 rounded-lg text-white"
               />
             </motion.div>
           </form>
