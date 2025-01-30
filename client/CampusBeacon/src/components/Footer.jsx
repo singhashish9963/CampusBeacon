4b7f5147fe88e22224a8069555168e40d33e98ed
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-30 21:02:06 +0530

    update contact us in footer

--- client/CampusBeacon/src/components/Footer.jsx
+++ client/CampusBeacon/src/components/Footer.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import { motion } from "framer-motion";
-import {HiLocationMarker, HiMail, HiPhone} from "react-icons/hi";
+import { HiLocationMarker, HiMail, HiPhone } from "react-icons/hi";
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-b from blue-900 to-black text-white py-7">
+    <div className="bg-gradient-to-b from-gray-900 to-black text-white py-8">
       <div className="container mx-auto grid grid-cols-1 md:grids-cols-3 gap-8 px-4">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <h3 className="text-xl font-bold text-blue mb-4">Contanct Us</h3>
+          <h3 className="text-2xl font-bold text-blue-400 mb-4">Contanct Us</h3>
           <div className="space-y-3">
-            <a href="mailto:campusbeacon0@gmail.com"
-            className="flex items-center hover:text-blue-400 transition-colors">
-                <HiMail className="mr-2" size={20} />
-                <span>campusbeacon0@gmail.com</span>
+            <a
+              href="mailto:campusbeacon0@gmail.com"
+              className="flex items-center hover:text-blue-600 transition-colors"
+            >
+              <HiMail className="mr-2" size={20} />
+              <span>campusbeacon0@gmail.com</span>
             </a>
             <a
-            href="tel:+919548999129"
-            className="flex items-center hover:text-blue-400 transition-colors">
-            <HiPhone className="mr-2" size={20}/>
-            <span>+91 9548999129</span>
+              href="tel:+919548999129"
+              className="flex items-center hover:text-blue-600 transition-colors"
+            >
+              <HiPhone className="mr-2" size={20} />
+              <span>+91 9548999129</span>
             </a>
-            <div className="flex items-center">
-              <HiLocationMarker className="mr-2" size={20}/>
-              <span>Motilal Nehru National Institute Of Technology, Prayagraj</span>
-            </div>
+            <a
+              href="https://maps.app.goo.gl/tMuCf5DjfXLF3YuDA"
+              className="flex items-center hover:text-blue-600 transition-colors"
+            >
+              <HiLocationMarker className="mr-2" size={20} />
+              <span>Motilal Nehru National Institute of Technology</span>
+            </a>
           </div>
         </motion.div>
-        <motion.div>
-          
-
-        </motion.div>
+        <motion.div></motion.div>
       </div>
     </div>
   );
