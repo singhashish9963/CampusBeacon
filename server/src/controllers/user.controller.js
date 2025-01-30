73f1130cf93823a1047967c75f306af05e3ce8ea
Ayush <ayushjadaun6@gmail.com>
2025-01-30 19:57:40 +0530

    add createUser

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -33,15 +33,31 @@ export const createUser= async(req,res)=>{
       "Production and Industrial Engineering",
       "Biotechnology",
     ];
-    if(!name) return new ApiResponse(400,"name is required");
-    if(!registration_number) return new ApiResponse(400,"registration is required");
-    if(!name) return new ApiResponse(400,"name is required");
-    if(!name) return new ApiResponse(400,"name is required");
-    if(!name) return new ApiResponse(400,"name is required");
-    if(!name) return new ApiResponse(400,"name is required");
+    if (!name) return res.status(400).json({ error: "Name is required" });
+    if (!registration_number)
+      return res.status(400).json({ error: "Registration number is required" });
+    if (!semester || !semesters.includes(semester))
+      return res.status(400).json({ error: "Valid semester is required" });
+    if (!branch || !branches.includes(branch))
+      return res.status(400).json({ error: "Valid branch is required" });
+    if (!hostel) return res.status(400).json({ error: "Hostel is required" });
+    if (!graduation_year)
+      return res.status(400).json({ error: "Graduation year is required" });
 
+    try{
+        const newUser= new UserActivation({
+            name,
+            registration_number,
+            semester,
+            branch,
+            hostel,
+            graduation_year,
+        });
+        const savedUser= await newUser.save();
+        return res.status(201).json(savedUser);
 
+    } catch(err){
+        return res.status(500).json({error:"Error saving user"})
+    }
+};
 
-
-
-}
