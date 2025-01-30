3b15559a054a7d19fcbbe4e730deb73e8bb742de
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-30 23:02:13 +0530

    Feature Card component complete

--- client/CampusBeacon/src/components/FeatureCard.jsx
+++ client/CampusBeacon/src/components/FeatureCard.jsx
@@ -7,7 +7,13 @@ const FeatureCard = ({ icon: Icon, title, descrip }) => {
       whileInView={{ opacity: 1, y: 0 }}
       whileHover={{ scale: 1.25 }}
       className="bg-gray-900/50 backdrop-blur-sm rounded-xl border border-pink-500/20 hover:border-pink-200/40 transition-all"
-    ></motion.div>
+    >
+      <div className="flex flex-col items-center text-center space-y-4 ">
+        <Icon className="h-11 w-20 text-pink-500" />
+        <h3 className="text-2xl font-bold text-white">{title}</h3>
+        <p className="text-gray-400">{description}</p>
+      </div>
+    </motion.div>
   );
 };
 
