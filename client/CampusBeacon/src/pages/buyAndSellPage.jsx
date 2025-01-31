e675cea0fddde857f275c69391c27e76014d1a7a
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-01-31 23:47:52 +0530

    backgroud of buy and sell

---
+++ client/CampusBeacon/src/pages/buyAndSellPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Marketplace =()=>{
+    return(
+        <div className="min-h-screen bg-gradient-to-br from-indigo-900 via-black to-purple-900 text-white">
+            <div className="container mx-auto p-8">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Marketplace;
