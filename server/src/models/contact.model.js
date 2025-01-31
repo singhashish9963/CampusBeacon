e4f41038a5465e4330700e345fda1d42d449371b
Ayush <ayushjadaun6@gmail.com>
2025-01-31 20:28:20 +0530

    create contact  model

--- server/src/models/contact.model.js
+++ server/src/models/contact.model.js
@@ -1,3 +1,33 @@
-import sequelize from "../db/db.js"
+import sequelize from "../db/db.js";
+import { DataTypes } from "sequelize";
 
-
+export const Contact = sequelize.define("Contact", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    designation: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+}, {
+    timestamps: true, 
+});
