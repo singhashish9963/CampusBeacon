e5e0abf7be23cc9975d5304f2369ed5bb0b7ab0c
Ayush <ayushjadaun6@gmail.com>
2025-02-01 22:23:08 +0530

    fix djgh error

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,8 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
    <App/>
