647ade4039a7e5f6db33796f65f5147d6bc617c3
Ayush <ayushjadaun6@gmail.com>
2025-02-01 11:38:15 +0530

    update wrong import

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,4 +1,4 @@
-import { message } from "../models/message.model";
+import { message } from "../models/message.model.js";
 import asyncHandler from "../utils/asyncHandler.js"
 import apiError from "../utils/apiError.js"
 import apiResponse from "../utils/apiResponse.js"
