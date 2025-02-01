214decce02131cf436a9d44569677962f1157e05
Ayush <ayushjadaun6@gmail.com>
2025-02-01 15:37:13 +0530

    add appwrite id for in user model for better validation

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,70 +1,80 @@
 import sequelize from "../db/db.js";
 import { DataTypes } from "sequelize";
 
-const users = sequelize.define("users", {
+
+
+const users = sequelize.define(
+  "users",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    appwrite_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, 
     },
     registration_number: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        unique: true,
-        index:true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      index: true,
+      // for faster search(can be used for fetching particular data in later versions and for chat)
     },
     semester: {
-        type: DataTypes.ENUM,
-        values: [
-            "First",
-            "Second",
-            "Third",
-            "Fourth",
-            "Fifth",
-            "Sixth",
-            "Seventh",
-            "Eighth",
-        ],
-        allowNull: false,
+      type: DataTypes.ENUM,
+      values: [
+        "First",
+        "Second",
+        "Third",
+        "Fourth",
+        "Fifth",
+        "Sixth",
+        "Seventh",
+        "Eighth",
+      ],
+      allowNull: false,
     },
     branch: {
-        type: DataTypes.ENUM,
-        values: [
-            "Electronics and Communication Engineering",
-            "Computer Science Engineering",
-            "Electrical Engineering",
-            "Mechanical Engineering",
-            "Civil Engineering",
-            "Engineering and Computational Mechanics",
-            "Chemical Engineering",
-            "Material Engineering",
-            "Production and Industrial Engineering",
-            "Biotechnology",
-        ],
-        allowNull: false,
+      type: DataTypes.ENUM,
+      values: [
+        "Electronics and Communication Engineering",
+        "Computer Science Engineering",
+        "Electrical Engineering",
+        "Mechanical Engineering",
+        "Civil Engineering",
+        "Engineering and Computational Mechanics",
+        "Chemical Engineering",
+        "Material Engineering",
+        "Production and Industrial Engineering",
+        "Biotechnology",
+      ],
+      allowNull: false,
     },
     hostel: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     graduation_year: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-   
-},{
-    timestamps:true,
-    indexes:[
-        {
-            unique:true,
-            fields:['registration_number']
-        },
-    ]
-}
+  },
+  {
+    timestamps: true,
+    indexes: [
+      {
+        unique: true,
+        fields: ["registration_number"],
+      },
+    ],
+  }
 );
 
 export default users;
