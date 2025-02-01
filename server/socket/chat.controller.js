92ec47adfabfb82af52490b677d440f797b97c90
Ayush <ayushjadaun6@gmail.com>
2025-02-01 12:02:45 +0530

    add handle joinChannel

--- server/socket/chat.controller.js
+++ server/socket/chat.controller.js
@@ -1,6 +1,3 @@
-import  message  from "../src/models/message.model.js";
-import users from "../src/models/user.model.js";
-
 class ChatController {
   constructor(io) {
     this.io = io;
@@ -9,8 +6,20 @@ class ChatController {
 
   initialize(socket) {
     console.log('User connected:', socket.id);  
+    this.handleJoinChannel(socket);
+    this.handleSendMessage(socket);
+    this.handleDisconnect(socket);
   }
 }
+
+ handleJoinChannel(socket) {
+    socket.on('joinChannel', (channelId) => {
+      socket.join(channelId.toString());
+      console.log(`User joined channel: ${channelId}`);
+    });
+  }
+
+
 
 export default ChatController;
 
