c3ce70c190d6998d25e1833d52f5d5357ba87e79
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-02-01 18:23:09 +0530

    add constant menu in SVBH

--- client/CampusBeacon/src/pages/HostelPages/SVBH.jsx
+++ client/CampusBeacon/src/pages/HostelPages/SVBH.jsx
@@ -1,1 +1,59 @@
 import React from "react";
+
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const menu = {
+  Monday: {
+    Breakfast: "Aalo Paratha, Aachar, Sauce, Chai, Bread, Jam, Banana, Sprouts",
+    Lunch: "Dal Tadka, Aalo Gobi Rasedar, Rice, Roti, Raita, Salad",
+    Snacks: "Chow Mein, Sauce, Hot Coffee",
+    Dinner: "Aalo Soya Methi, Fry Arhar Dal, Rice, Roti, Sewain, Salad",
+  },
+  Tuesday: {
+    Breakfast: "Pav Bhaji, Hot Coffee, Bread, Jam, Banana, Sprouts",
+    Lunch: "Dal Makhani, Aalo Jeera, Rice, Roti, Raita, Salad",
+    Snacks: "Vada Pav, Chai",
+    Dinner: "Aalo Tamatar Rasedar, Methi Paratha, Rice, Custard, Salad",
+  },
+  Wednesday: {
+    Breakfast:
+      "Idli, Sambar, Nariya Chutni, Bournvita, Bread, Jam, Banana, Sprouts",
+    Lunch: "Kali Masoor Dal, Mix Veg, Rice, Roti, Raita, Salad",
+    Snacks: "Aalo Sandwich, Chai",
+    Dinner: "Palak Paneer, Chana Dal, Roti, Rice, Gulab Jamun, Salad",
+  },
+  Thursday: {
+    Breakfast: "Paneer Paratha, Milk, Bread, Jam, Banana, Sprouts",
+    Lunch: "Khadi, Aalo Fry, Rice, Roti, Salad",
+    Snacks: "Tikiyan, Chutni, Bournvita",
+    Dinner: "Mix Dal, Aalo Gobhi, Roti, Rice, Custard, Salad",
+  },
+  Friday: {
+    Breakfast: "Vada, Sambar, Nariya Chutni, Milk, Bread, Jam, Banana, Sprouts",
+    Lunch: "Malai Kofta, Arhar Dal, Rice, Roti, Salad",
+    Snacks: "Burger, Sauce, Hot Coffee",
+    Dinner: "Mushroom, Dal Makhani, Roti, Rice, Ice Cream, Salad",
+  },
+  Saturday: {
+    Breakfast: "Samosa, Chola, Milk, Bread, Jam, Banana, Sprouts",
+    Lunch: "Shahi Paneer, Mix Dal, Rice, Roti, Gulab Jamun, Salad",
+    Snacks: "—Break—",
+    Dinner: "Veg Biryani/Pulao, Boondi Raita, Chutni",
+  },
+  Sunday: {
+    Breakfast:
+      "Chana Masala, Poodi, Sooji Halwa, Milk, Bread, Jam, Banana, Sprouts",
+    Lunch: "Chola, Bhatura, Rice, Roti, Dahi Vada, Salad",
+    Snacks: "Pasta, Sauce, Chai",
+    Dinner: "Litti Chokha, Arhar Dal, Rice, Kheer, Salad",
+  },
+};
+
