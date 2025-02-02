bd094a6f73d82ae84fbb10080efba38aad6803bc
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-02-02 13:57:44 +0530

    adding comments in profile page

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,8 +2,8 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-   <App/>
-  </StrictMode>
-);
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+    <App/>
+    </StrictMode>
+  );
