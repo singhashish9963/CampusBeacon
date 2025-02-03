fe0a6d6a20161cba98fafff8981d98c868adfdcf
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-02-03 18:20:57 +0530

    minor changes

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,10 +2,11 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Eatries from "./pages/Eatries.jsx";
 
   createRoot(document.getElementById("root")).render(
     <StrictMode>
-    <App/>
-  
+    {/* <App/> */}
+    <Eatries />
     </StrictMode>
   );
