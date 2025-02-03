356354c11e458ffae1415a09f377f938c431f0c2
Ayush <ayushjadaun6@gmail.com>
2025-02-03 10:17:51 +0530

    create a check for auth in profile

--- client/CampusBeacon/src/contexts/profileContext.jsx
+++ client/CampusBeacon/src/contexts/profileContext.jsx
@@ -2,5 +2,21 @@ import React, {useCallback,useContext,useState,createContext} from "react";
 import { handleApiCall } from "../services/userService";
 
 
+
 const profileContext= createContext(null);
 
+export const profileProvider= ({ children })=>{
+  const checkUser = () => {
+    const userStorage = localStorage.getItem("authUser");
+    if (userStorage) {
+      try {
+        return JSON.parse(userStorage);
+      } catch (e) {
+        console.error("Error parsing stored user:", e);
+      }
+    }
+    return null;
+  };
+  
+
+}
