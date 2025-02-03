0d411157edfe77ceb6cfa83707f19dc0ff4f8942
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-02-03 18:27:05 +0530

    delete eatries page

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Eatries from "./pages/Eatries.jsx";
 
   createRoot(document.getElementById("root")).render(
     <StrictMode>
