9d6eb8fa054bca7e76d53cc434a2b2d2cc516325
Ayush <ayushjadaun6@gmail.com>
2025-02-03 10:03:07 +0530

    add community page href in home page, add href tag in feature card

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,10 +2,10 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import CommunityPage from "./pages/communityPage.jsx";
+
   createRoot(document.getElementById("root")).render(
     <StrictMode>
-    {/* <App/> */}
-    <CommunityPage />
+    <App/>
+  
     </StrictMode>
   );
