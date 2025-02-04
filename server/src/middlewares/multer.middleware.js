19e432069e1023a797f246319e9a894a929913d5
Ayush <ayushjadaun6@gmail.com>
2025-02-04 23:10:50 +0530

    fix file path in multer

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -2,27 +2,26 @@ import multer, { diskStorage } from "multer";
 import { extname as _extname } from "path";
 
 const storage = diskStorage({
-
   destination: function (_req, _file, cb) {
-    cb(null, ".public/temp");
+    cb(null, "./public/temp"); 
   },
- 
+
   filename: function (_req, file, cb) {
-    cb(null, file.originalname); 
+    cb(null, file.originalname);
   },
 });
 
 export const upload = multer({
   storage: storage,
   fileFilter: function (_req, file, cb) {
-
     const filetypes = /jpeg|jpg|png|gif/;
     const extname = filetypes.test(_extname(file.originalname).toLowerCase());
     const mimetype = filetypes.test(file.mimetype);
+
     if (extname && mimetype) {
       cb(null, true);
     } else {
-      cb(new Error("File type not supported"), false);
+      cb(new ApiError("File type not supported", 400), false);
     }
   },
 });
