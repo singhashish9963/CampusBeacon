68035636525e048ada4ceaa68c61de528d258fd0
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-02-04 20:36:49 +0530

    minor changes

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,6 +2,8 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Marketplace from "./pages/buyAndSellPage.jsx";
+import LostAndFound from "./pages/lostAndFound.jsx";
 
   createRoot(document.getElementById("root")).render(
     <StrictMode>
