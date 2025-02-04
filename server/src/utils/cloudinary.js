2dd6fa2f72a192bbf71173c422b15cfc2543d1b8
Ayush <ayushjadaun6@gmail.com>
2025-02-05 00:54:57 +0530

    use async hadnler and dotenv

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,36 +1,34 @@
 import { v2 as cloudinary } from "cloudinary";
 import asyncHandler from "./asyncHandler.js";
 import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config()
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dmvsj5vla",
-  api_key: process.env.CLOUDINARY_API_KEY || "957488376714669",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret:
-    process.env.CLOUDINARY_API_SECRET || "3_3dj13Z4ecrKUAD14y3FLVOVR0",
+    process.env.CLOUDINARY_API_SECRET,
 });
 
-export const uploadImageToCloudinary = async (localFilePath) => {
-  try {
-    if (!localFilePath) {
-      console.log("No file path provided");
-      return null;
-    }
-
-    console.log("Attempting to upload file:", localFilePath);
-
-    const result = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
-    });
-
-    console.log("Cloudinary upload successful:", result);
-    fs.unlinkSync(localFilePath);
-
-    return result;
-  } catch (error) {
-    console.error("Cloudinary upload error:", error);
-
-    if (fs.existsSync(localFilePath)) {
-      fs.unlinkSync(localFilePath);
-    }
-    throw error;
+export const uploadImageToCloudinary = asyncHandler(async (localFilePath) => {
+  if (!localFilePath) {
+    console.log("No file path provided");
+    return null;
   }
-};
+
+  console.log("Attempting to upload file:", localFilePath);
+
+  const result = await cloudinary.uploader.upload(localFilePath, {
+    resource_type: "auto",
+  });
+
+  console.log("Cloudinary upload successful:", result.secure_url);
+
+  fs.unlink(localFilePath, (err) => {
+    if (err) console.error("Error deleting file:", err);
+  });
+
+  return result.secure_url;
+});
