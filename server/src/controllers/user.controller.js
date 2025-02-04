cca403833358526a76016dd87530ba249ac10407
Ayush <ayushjadaun6@gmail.com>
2025-02-04 11:16:09 +0530

    create loginUser

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import ApiError from "../utils/apiError.js"
 import ApiResponse from "../utils/apiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
 
 
-export const registerUser=asyncHandler(async (req,res)=>{
+export const registerUser=asyncHandler(async (req,res,next)=>{
     const {email,password}= req.body
 
     const existingUser= await User.findOne({where: {email}})
@@ -19,3 +20,25 @@ export const registerUser=asyncHandler(async (req,res)=>{
 
     res.status(201).json(new ApiResponse(201,{userId: newUser.id},"User registered succesfully "))
 })
+
+export const loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        return next(new ApiError("Invalid email or password", 400));
+    }
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return next(new ApiError("Invalid email or password", 400));
+    }
+
+    const token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+
+    res.status(200).json(new ApiResponse(200, { token }, "Login successful"));
+});
