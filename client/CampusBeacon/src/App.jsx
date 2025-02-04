ffd12c25183d4302a4de94323880f8ec4afb2050
Ayush <ayushjadaun6@gmail.com>
2025-02-04 12:24:32 +0530

    update navbar to only show home and login

--- client/CampusBeacon/src/App.jsx
+++ client/CampusBeacon/src/App.jsx
@@ -9,10 +9,11 @@ import ProfilePage from "./pages/profilePage.jsx";
 import Marketplace from "./pages/buyAndSellPage.jsx";
 import LostAndFound from "./pages/lostAndFound.jsx";
 import SVBH from "./pages/HostelPages/SVBH.jsx";
-import DJGH from "./pages/HostelPages/DJGH.jsx"
-
+import DJGH from "./pages/HostelPages/DJGH.jsx";
 import CommunityPage from "./pages/communityPage.jsx";
 import { ProfileProvider } from "./contexts/profileContext.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
 function App() {
   return (
     <AuthProvider>
@@ -24,12 +25,14 @@ function App() {
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginSignup />} />
-                <Route path="/lost-found" element={<LostAndFound />} />
-                <Route path="/marketplace" element={<Marketplace />} />
-                <Route path="/SVBH" element={<SVBH />} />
-                <Route path="/DJGH" element={<DJGH />} />
-                <Route path="/Community" element={<CommunityPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/profile" element={<ProfilePage />} />
+                  <Route path="/marketplace" element={<Marketplace />} />
+                  <Route path="/lost-found" element={<LostAndFound />} />
+                  <Route path="/SVBH" element={<SVBH />} />
+                  <Route path="/DJGH" element={<DJGH />} />
+                  <Route path="/Community" element={<CommunityPage />} />
+                </Route>
                 <Route path="*" element={<Navigate to="/" replace />} />
               </Routes>
             </main>
