472d63834485be93a3b4c268a92cdc06b33e72ea
Ayush <ayushjadaun6@gmail.com>
2025-02-04 11:39:58 +0530

    update authMiddleware to take cookie

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,29 +1,20 @@
 import jwt from "jsonwebtoken";
 import ApiError from "../utils/apiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-export const authMiddleware = (req, res, next) => {
-  try {
- 
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      throw new ApiError("No token provided", 401);
-    }
+export const authMiddleware = asyncHandler(async (req, res, next) => {
+  const token = req.cookies.token;
 
-   
-    const token = authHeader.split(" ")[1];
-    if (!token) {
-      throw new ApiError("Invalid token format", 401);
-    }
-
-  
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-   
-    req.user = decoded;
-    next();
-  } catch (error) {
-    next(new ApiError(error.message || "Authentication failed", 401));
+  if (!token) {
+    throw new ApiError("Not authenticated. Please log in.", 401);
   }
-};
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+
+  req.user = decoded;
+
+  next();
+});
 
 export default authMiddleware;
