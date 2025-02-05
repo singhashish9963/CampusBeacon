b65ac932f20a2a1a9187d8d9380905e10986c36f
Ayush <ayushjadaun6@gmail.com>
2025-02-05 21:22:15 +0530

    crearte getmessages

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -17,3 +17,23 @@ const getChannels = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, channels, "Channels fetched successfully"));
 });
+
+const getMessages = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+
+  const channel = await Channel.findByPk(channelId);
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  const messages = await Message.findAll({
+    where: { channelId },
+    order: [["timestamp", "DESC"]],
+    limit: 50, 
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, messages, "Messages fetched successfully"));
+});
