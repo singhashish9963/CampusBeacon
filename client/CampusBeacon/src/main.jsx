005721010d42a50eb6ba1333ca1cd914395d3770
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-02-07 01:27:14 +0530

    add eatries page

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,11 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import CollegeEateries from "./pages/eatries.jsx";
 
 
   createRoot(document.getElementById("root")).render(
     <StrictMode>
-    <App/>
-   
+    {/* <App/> */}
+    <CollegeEateries />
     </StrictMode>
   );
