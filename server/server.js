872ef0915d5caf0cc8aca5675810961b98c9c57f
Ayush <ayushjadaun6@gmail.com>
2025-02-08 00:58:56 +0530

    .

--- server/server.js
+++ server/server.js
@@ -20,23 +20,14 @@ const PORT = process.env.PORT || 5000;
 const httpServer = createServer(app);
 
 // Middleware setup
-app.use(cookieParser());
-const allowedOrigins = [
-  "https://campus-beacon.vercel.app",
-];
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://campus-beacon.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Socket.io setup
