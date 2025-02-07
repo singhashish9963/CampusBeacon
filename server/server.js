10db16e45f453684861a2562992c7511dfdf4e8f
Ayush <ayushjadaun6@gmail.com>
2025-02-07 23:39:33 +0530

    FIX CORS url

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const httpServer = createServer(app);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "https://campusbeacon.onrender.com",
+    origin: process.env.CORS_ORIGIN || "https://campus-beacon.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
