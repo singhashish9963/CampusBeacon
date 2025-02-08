7378138474b0eab82f128064f37ebf69651a40f3
Ayush <ayushjadaun6@gmail.com>
2025-02-08 09:58:22 +0530

    final commit

--- client/CampusBeacon/src/pages/HostelPages/SVBH.jsx
+++ client/CampusBeacon/src/pages/HostelPages/SVBH.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Utensils, Phone, Mail, Bell, Wrench } from "lucide-react";
 import { FaUsersGear } from "react-icons/fa6";
-
+import axios from "axios";
 
 const sendEmailForComplaint = async (
   emails,
