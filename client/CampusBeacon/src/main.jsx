cfb6558939f1ac914c62864fa124145fb123bfec
Ayush <ayushjadaun6@gmail.com>
2025-03-09 22:43:00 +0530

    fix merge issue

--- client/CampusBeacon/src/main.jsx
+++ client/CampusBeacon/src/main.jsx
@@ -2,8 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ResourcesPage from "./pages/ResourceHub.jsx";
-import CommunityPage from "./pages/communityPage.jsx";
+
 
   createRoot(document.getElementById("root")).render(
     <StrictMode>
