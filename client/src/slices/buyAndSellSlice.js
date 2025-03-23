f6f3d37455c9bb12997176c7778449b314d51636
Ayush <ayushjadaun6@gmail.com>
2025-03-24 02:00:50 +0530

    name change

--- client/src/slices/buyAndSellSlice.js
+++ client/src/slices/buyAndSellSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios from "axios"
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
