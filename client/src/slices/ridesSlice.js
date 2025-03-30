f112ad60e5a525aa52fd83cd09fafb7b460ed7b7
Ayush <ayushjadaun6@gmail.com>
2025-03-30 09:37:10 +0530

    deploy push

--- client/src/slices/ridesSlice.js
+++ client/src/slices/ridesSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://campusbeacon.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
